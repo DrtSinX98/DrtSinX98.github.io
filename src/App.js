d5fcfad3f3386083be1ceb8d0305dc55bc0d590b
DrtSinX98 <pritish@stag-os.org>
2023-04-08 00:00:26 +0530

    Add Spinner animation
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react"
+import { Container, Spinner } from "react-bootstrap";
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -8,17 +8,34 @@ import Header, { useActiveState } from "./components/Header"
 
 function App() {
     const { active, handleSelect } = useActiveState();
+    const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call to fetch data
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
     return (
         <div>
             <Header active={active} handleSelect={handleSelect} />
+            {loading ? (
+            <div className="spinner-container">
+                <Spinner className="spinner" />
+            </div>
+            ) : (
+            <div>
             <Container>
             {active === 'home' && <Home />}
             {active === 'about' && <About />}
             {active === 'projects' && <Projects />}
             {active === 'contact' && <Contact />}
             </Container>
+            </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default App
