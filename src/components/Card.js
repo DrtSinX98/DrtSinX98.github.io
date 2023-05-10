52a60d092f80590dc567e74eeddabd1f950d0f13
DrtSinX98 <pritish@stag-os.org>
2023-05-10 07:13:33 +0000

    Get rid of warnings
    * Assign key prop with index parameter
    * Fix 'ul' nested inside 'p'
    
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,20 +4,18 @@ import { Image } from "react-bootstrap";
 import data from "../data.json";
 
 function Card() {
-  return data.map((item) => (
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div key={item.title} className="card">
+  return data.map((item, index) => (
+    <div className="col-lg-4 col-md-6 mb-4" key={index}>
+      <div className="card">
         <Image className="card-img-top" src={item.image} alt="Card cap" />
         <div className="card-body">
           <h5 className="card-title">{item.title}</h5>
-          <p className="card-text">
-            <ul>
+            <ul className="card-text">
               {item.item1 && <li>{item.item1}</li>}
               {item.item2 && <li>{item.item2}</li>}
               {item.item3 && <li>{item.item3}</li>}
               {item.item4 && <li>{item.item4}</li>}
             </ul>
-          </p>
           <Button variant="secondary" size="sm" href={item.more}>
             More..
           </Button>
