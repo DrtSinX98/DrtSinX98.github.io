d7ec4f29fa510153e3a3492fe4d2130a71d8b3ec
DrtSinX98 <pritish@stag-os.org>
2023-05-10 11:05:51 +0000

    Isolate Cards component
    * using styled jsx to write css of cards inside the jsx
    
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,17 +9,57 @@ function Cards() {
         <Card.Img variant="top" src={item.image} alt="Card cap" />
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
-            <ul className="card-text">
-              {item.item1 && <li>{item.item1}</li>}
-              {item.item2 && <li>{item.item2}</li>}
-              {item.item3 && <li>{item.item3}</li>}
-              {item.item4 && <li>{item.item4}</li>}
-            </ul>
+          <ul className="card-text">
+            {item.item1 && <li>{item.item1}</li>}
+            {item.item2 && <li>{item.item2}</li>}
+            {item.item3 && <li>{item.item3}</li>}
+            {item.item4 && <li>{item.item4}</li>}
+          </ul>
           <Button variant="secondary" size="sm" href={item.more}>
             More..
           </Button>
         </Card.Body>
       </Card>
+      <style>
+        {`
+        .card {
+          background-color: #eeeafc;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          transition: 0.3s;
+        }
+        
+        .card-img-top {
+          height: 250px;
+          object-fit: cover;
+        }
+        
+        .card-title {
+          font-size: 24px;
+          font-weight: bold;
+        }
+        
+        .card-text {
+          font-size: 18px;
+          line-height: 1.5;
+        }
+        
+        .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+        
+        .btn-secondary {
+          background-color: #c91574;
+          font-size: 18px;
+          padding: 6px 12px;
+          margin: 0;
+        }
+        
+        .btn-secondary:hover {
+          background-color: #301934;
+          cursor: pointer;
+        }
+        `}
+      </style>
     </Col>
   ));
 }
