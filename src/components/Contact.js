6fe8160af20a09c24cc4b51f15459ef7d99b2490
Pritish Joshi <pritish@stag-os.org>
2024-07-14 18:50:13 +0530

    Update Contacts page
    Signed-off-by: Pritish Joshi <pritish@stag-os.org>

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Image , Container, Row } from "react-bootstrap";
+import { Col, Image , Container, Row, Card } from "react-bootstrap";
 import ct from "../images/ct.svg";
 
 function Contact() {
@@ -13,10 +13,84 @@ function Contact() {
       </Col>
       <Col>
       <h1>Reach out <span className="pink">to me!</span></h1>
-      <p className="lead">I'm available on Linkedin, Twitter, Telegram, Whatsapp and <span className="pink">Reddit</span>Not on Facebook and Instagram though because of a certain <span className="pink">Lizard</span> XD. <br/>Below you can find my contact details, feel free to reach out.</p>
+      <p className="lead">I'm available on <span className="pink">Linkedin</span>, Twitter, Telegram, Whatsapp, Discord, <span className="pink">Reddit</span> and Instagram. <br/>Below you can find my contact details, feel free to reach out.</p>
       </Col>
       </Row>
       <hr className="my-4" />
+      <Row>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://www.linkedin.com/in/pritish-joshi-b870bb242" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">LinkedIn</Card.Title>
+          <Card.Text>
+            Connect with me on LinkedIn
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://x.com/rjpritish" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">Twitter</Card.Title>
+          <Card.Text>
+            Follow me on Twitter (<span className="pink">X</span>)
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://t.me/rj_pritish" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">Telegram</Card.Title>
+          <Card.Text>
+            Chat with me on Telegram
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://whatsapp.com/channel/0029VaGxfUGDp2QAuZaTqp1G" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">Whatsapp</Card.Title>
+          <Card.Text>
+            Join me on Whatsapp
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://www.instagram.com/the_istreet" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">Instagram</Card.Title>
+          <Card.Text>
+            Join my channel on Instagram
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      <Col lg={4} md={6} className="mb-4">
+      <a href="https://discordapp.com/users/rjpritish_08301" target="_blank" rel="noreferrer">
+      <Card>
+        <Card.Body>
+          <Card.Title className ="pink">Discord</Card.Title>
+          <Card.Text>
+            Message me on Discord
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </a>
+      </Col>
+      </Row>
       <style>
         {`
           #ct-img {
@@ -37,7 +111,29 @@ function Contact() {
           .pink {
             color: #c91574;
           }
-
+          
+          .card {
+            background-color: #eeeafc;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            transition: 0.3s;
+          }
+          
+          .card-title {
+            font-size: 24px;
+            font-weight: bold;
+          }
+          
+          .card-text {
+            font-size: 18px;
+            line-height: 1.5;
+          }
+          
+          .card:hover {
+            background-color: #301934;
+            color: white;
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+          }
+          
           @media (max-width: 767px) {
             .image-p {
               display: flex;
