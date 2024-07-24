e2d3a7c3fd2068b06e79e522f4259451d1b38f22
Pritish Joshi <pritish@stag-os.org>
2024-07-24 23:21:08 +0530

    Migrate from CRA to Vite
    * Create-react-app (CRA) has been abandoned
    * Migrating the project to Vite for futureproofing
    * Also update npm packages
    
    Signed-off-by: Pritish Joshi <pritish@stag-os.org>

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
+import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(<App />);
