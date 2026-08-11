819fbd012e7f771877ff4c74ab357dade3efda17
Pritish Joshi <rjpritish@gmail.com>
2026-08-12 00:12:33 +0200

    Gallery: Use jsdelivr CDN for hosting images

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
+export default defineConfig(({ command, mode }) => {
   return {
+    base: command === 'build' ? 'https://cdn.jsdelivr.net/gh/DrtSinX98/DrtSinX98.github.io@gh-pages/' : '/',
     build: {
       outDir: 'build',
     },
